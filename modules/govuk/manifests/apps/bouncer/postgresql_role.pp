557ba6087e7365cd85d5dcf291676871cbd0d15a
Isabell Long <isabell@issyl0.co.uk>
2017-10-26 14:18:03 +0100

    Make bouncer::postgresql_role handle being on RDS
    - For these queries to work on AWS, they need to specify a hostname
      (transition-postgresql-primary in this case) where the database lives.
      So, wrap the queries with hostnames in an $::aws_migration
      conditional, and leave the existing ones as is. This is a temporary
      FIXME (famous last words) until we're fully on AWS.

--- modules/govuk/manifests/apps/bouncer/postgresql_role.pp
+++ modules/govuk/manifests/apps/bouncer/postgresql_role.pp
@@ -1,10 +1,26 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::bouncer::postgresql_role ( $password = '' ){
+class govuk::apps::bouncer::postgresql_role (
+  $password = '',
+  $rds = undef,
+  ){
   $bouncer_role = 'bouncer'
   $db           = 'transition_production'
+  $hostname     = 'transition-postgresql-primary'
+
+  # For RDS, we have to specify hostnames in psql queries.
+  if $::aws_migration {
+    $alter_default_privileges_bouncer_query = "psql -h ${hostname} -d ${db} -c 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ${bouncer_role}'"
+    $bouncer_can_select_everything_query = "psql -h ${hostname} -d ${db} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${bouncer_role}'"
+    $test_privileges_are_set_for_one_table = "psql -h ${hostname} -d ${db} -c '\\dp' | grep -q 'bouncer=r/'"
+  } else {
+    $alter_default_privileges_query = "psql -d ${db} -c 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ${bouncer_role}'"
+    $bouncer_can_select_everything_query = "psql -d ${db} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${bouncer_role}'"
+    $test_privileges_are_set_for_one_table = "psql -d ${db} -c '\\dp' | grep -q 'bouncer=r/'"
+  }
 
   postgresql::server::role { $bouncer_role:
     password_hash => postgresql_password($bouncer_role, $password),
+    rds           => $rds,
   }
 
   postgresql::server::database_grant { "${bouncer_role} can connect to ${db}":
@@ -15,8 +31,8 @@ class govuk::apps::bouncer::postgresql_role ( $password = '' ){
   }
 
   exec { "${bouncer_role} will be able to SELECT from all yet-to-be-created tables in ${db}":
-    command => "psql -d ${db} -c 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ${bouncer_role}'",
-    unless  => "psql -d ${db} -c '\\ddp' | grep -q 'bouncer=r/'",
+    command => $alter_default_privileges_query,
+    unless  => $test_privileges_are_set_for_one_table,
     user    => 'postgres',
     require => Postgresql::Server::Database_grant["${bouncer_role} can connect to ${db}"],
   }
@@ -25,9 +41,8 @@ class govuk::apps::bouncer::postgresql_role ( $password = '' ){
   # https://github.com/puppetlabs/puppetlabs-postgresql/blob/master/CHANGELOG.md#2015-03-10---supported-release-420
   # https://github.com/puppetlabs/puppetlabs-postgresql/pull/405
   exec { "${bouncer_role} can SELECT from all existing tables in ${db}":
-    command => "psql -d ${db} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${bouncer_role}'",
-    # This only tests that the privilege is set for at least one table, not all of them:
-    unless  => "psql -d ${db} -c '\\dp' | grep -q 'bouncer=r/'",
+    command => $bouncer_can_select_everything_query,
+    unless  => $test_privileges_are_set_for_one_table,
     user    => 'postgres',
     require => Exec["${bouncer_role} will be able to SELECT from all yet-to-be-created tables in ${db}"],
   }
